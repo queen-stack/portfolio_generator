c945071cf28cc2d6f63371f2919ebba9516966d6
JQueen <queen_jenf@yahoo.com>
2020-09-17 18:48:38 -0700

    final define of questions

--- app.js
+++ app.js
@@ -39,13 +39,19 @@ const promptUser = () => {
         }
     },
     {
-        type: 'input',
-        name: 'about',   
-        message: 'Provide some information about yourself:'
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
     }
   ]);
 };
-//promptUser().then(answers => console.log(answers));
 
 const promptProject = portfolioData => {
     console.log(`
