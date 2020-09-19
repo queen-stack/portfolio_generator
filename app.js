eed4f152447ff23cebf4400eb297414b03a898f0
JQueen <queen_jenf@yahoo.com>
2020-09-18 18:34:58 -0700

    able to write to file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const inquirer = require('inquirer')
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 // const profileDataArgs = process.argv.slice(2);
-// const [name, github] = profileDataArgs;
+ //const [name, github] = profileDataArgs;
 
-  //fs.writeFile('index.html', generatePage(name, github), err => {
-    //if (err) throw err;
+  // fs.writeFile('index.html', generatePage(name, github), err => {
+  //   if (err) throw err;
   
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-//   });
+  //  console.log('Portfolio complete! Check out index.html to see the output!');
+  //  });
 // console.log(inquirer)
 const promptUser = () => {
     return inquirer.prompt([
@@ -133,5 +133,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
